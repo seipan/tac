955a0bdd7f8ae2dd1d1d478daad667d894081a41
seipan <yamasakipann0218@gmail.com>
2023-11-16 02:47:18 +0900

    feat : post sdk

--- terraform/sdk/todo.go
+++ terraform/sdk/todo.go
@@ -1,9 +1,27 @@
 package sdk
 
-const (
-	host = "http://localhost:8091"
+import (
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
 )
 
-func Post() {
+const (
+	host = "http://localhost:8091/add"
+)
 
+func Post(content string) {
+	form := url.Values{}
+	form.Add("title", content)
+	req, err := http.NewRequest("POST", host, strings.NewReader(form.Encode()))
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	_, err = http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
